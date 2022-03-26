f422a59d0b36b9f840fe1587e9499cbd1f3bac7b
Valdemar Høgsvig Lorenzen <creme3sandwich@gmail.com>
2022-03-26 15:06:39 +0100

    fixed the indexOutOfBounds bugs in extractMin and tested with all files

--- part1/src/PQHeap.java
+++ part1/src/PQHeap.java
@@ -1,8 +1,19 @@
+/*
+ * Bastian Graver (...)
+ * Ian Andersen (...)
+ * Valdemar Lorenzen (valor21@student.sdu.dk)
+ *
+ * Aflevering af delprojekt 1.
+ */
+
 import java.util.ArrayList;
 
 public class PQHeap implements PQ {
     private ArrayList<Element> queue;
 
+    /*
+     * Returns a new, empty, `PQHeap`.
+     */
     public PQHeap() {
         queue = new ArrayList<Element>();
     }
@@ -12,9 +23,15 @@ public class PQHeap implements PQ {
      * Pre: the priority queue cannot be empty.
      */
     public Element extractMin() {
-        Element min = queue.get(0);
-        queue.set(0, queue.remove(queue.size() - 1));
-        minHeapify(0);
+        Element min;
+
+        if (queue.size() > 1) {
+            min = queue.get(0);
+            queue.set(0, queue.remove(queue.size() - 1));
+            minHeapify(0);
+        } else
+            min = queue.remove(0);
+
         return min;
     }
 
@@ -25,18 +42,30 @@ public class PQHeap implements PQ {
         minHeapInsert(e);
     }
 
+    /*
+     * Returns the index of the left child of the element at the given index.
+     */
     private int leftIdx(int i) {
         return 2 * i + 1;
     }
 
+    /*
+     * Returns the index of the right child of the element at the given index.
+     */
     private int rightIdx(int i) {
         return 2 * i + 2;
     }
 
+    /*
+     * Returns the index of the parent of the element at the given index.
+     */
     private int parentIdx(int i) {
         return (i - 1) / 2;
     }
 
+    /*
+     * Inserts the given element into the priority queue.
+     */
     private void minHeapInsert(Element elem) {
         queue.add(elem);
         int i = queue.size() - 1;
@@ -50,6 +79,9 @@ public class PQHeap implements PQ {
         }
     }
 
+    /*
+     * Orders the elements in the priority queue correctly.
+     */
     private void minHeapify(int i) {
         int l = leftIdx(i);
         int r = rightIdx(i);
@@ -75,8 +107,18 @@ public class PQHeap implements PQ {
         }
     }
 
-    /* Overrides */
+    /*
+     * Returns whether the priority queue is empty or not.
+     */
+    public boolean isEmpty() { 
+        return (queue.size() == 0);
+    }
 
+    // Overrides //
+
+    /*
+     * Returns a textual representation of the priority queue.
+     */
     public String toString() {
         String str = "";
 
--- part1/src/Testing.java
+++ part1/src/Testing.java
@@ -11,19 +11,19 @@ public class Testing {
             pq.insert(new Element(nextInt, "data: " + nextInt));
         }
 
-        //pq.insert(new Element(0, "data1"));
-        //pq.insert(new Element(-1, "data2"));
-        //pq.insert(new Element(1, "data3"));
-        //pq.insert(new Element(-2, "data4"));
-        //pq.insert(new Element(3, "data5"));
-        //pq.insert(new Element(0, "data?"));
-        System.out.println(pq.extractMin().getKey());
-        System.out.println("---------");
-        System.out.println(pq);
+        if (!pq.isEmpty()) {
+            System.out.println("---------");
+            System.out.println(pq);
 
-        //pq.extractMin();
+            System.out.println("Extracting minimum value:");
+            System.out.println(pq.extractMin());
+            System.out.println("---------");
+            System.out.println("New PQ:");
+            System.out.println(pq);
+        }
 
-        //System.out.println(pq);
+        while (!pq.isEmpty())
+            System.out.println(pq.extractMin().getData());
     }
 }
 
