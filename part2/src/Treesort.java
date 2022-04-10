1f8ebf9f93608fa7a907a89a32423b7a12d53aec
ifAndersen <ifa_ian@hotmail.com>
2022-04-10 10:37:44 +0200

    Created Treesort from the testing class

--- part2/src/Treesort.java
+++ part2/src/Treesort.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+public class Treesort {
 
-public class Testing {
-    public static void main(String[] args) {
+    public void treesort() {
+
         DictBinTree dict = new DictBinTree();
         Scanner scanner = new Scanner(System.in);
 
+        //Reads input and inserts in the DictBintree
         while (scanner.hasNextInt()) {
             dict.insert(scanner.nextInt());
         }
 
+        //Prints the resulting DictBinTree in-order
         for (int n : dict.orderedTraversal())
             System.out.println(n);
         
         scanner.close();
     }
+    
 }
-
